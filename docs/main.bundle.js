1d88ae48c4d2ad6e9355d440a30b61d62355f16c
Matt Payne <Payne@MattPayne.org>
2018-07-23 06:21:24 -0500

    ng build; rm -rf docs; cp -r dist docs

--- docs/main.bundle.js
+++ docs/main.bundle.js
@@ -60,7 +60,7 @@ var AppComponent = /** @class */ (function () {
         this.title = 'Coffee Drinkers App';
     }
     AppComponent = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
             selector: 'app-root',
             template: __webpack_require__("../../../../../src/app/app.component.html"),
             styles: [__webpack_require__("../../../../../src/app/app.component.css")]
@@ -82,12 +82,15 @@ var AppComponent = /** @class */ (function () {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/esm5/forms.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_common_http__ = __webpack_require__("../../../common/esm5/http.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4_angular_in_memory_web_api__ = __webpack_require__("../../../../angular-in-memory-web-api/index.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__in_memory_drinker_data_service_service__ = __webpack_require__("../../../../../src/app/in-memory-drinker-data-service.service.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__coffee_drinker_coffee_drinker_component__ = __webpack_require__("../../../../../src/app/coffee-drinker/coffee-drinker.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__list_coffee_drinkers_list_coffee_drinkers_component__ = __webpack_require__("../../../../../src/app/list-coffee-drinkers/list-coffee-drinkers.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__coffee_drinkers_service__ = __webpack_require__("../../../../../src/app/coffee-drinkers.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__swimlane_ngx_datatable__ = __webpack_require__("../../../../@swimlane/ngx-datatable/release/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__swimlane_ngx_datatable___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_4__swimlane_ngx_datatable__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5_angular_in_memory_web_api__ = __webpack_require__("../../../../angular-in-memory-web-api/index.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__in_memory_drinker_data_service_service__ = __webpack_require__("../../../../../src/app/in-memory-drinker-data-service.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__intra_component_message_service_service__ = __webpack_require__("../../../../../src/app/intra-component-message-service.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_9__coffee_drinker_coffee_drinker_component__ = __webpack_require__("../../../../../src/app/coffee-drinker/coffee-drinker.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_10__list_coffee_drinkers_list_coffee_drinkers_component__ = __webpack_require__("../../../../../src/app/list-coffee-drinkers/list-coffee-drinkers.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_11__coffee_drinkers_service__ = __webpack_require__("../../../../../src/app/coffee-drinkers.service.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -104,27 +107,30 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
+
+
 var AppModule = /** @class */ (function () {
     function AppModule() {
     }
     AppModule = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["E" /* NgModule */])({
+        Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["NgModule"])({
             declarations: [
-                __WEBPACK_IMPORTED_MODULE_6__app_component__["a" /* AppComponent */],
-                __WEBPACK_IMPORTED_MODULE_7__coffee_drinker_coffee_drinker_component__["a" /* CoffeeDrinkerComponent */],
-                __WEBPACK_IMPORTED_MODULE_8__list_coffee_drinkers_list_coffee_drinkers_component__["a" /* ListCoffeeDrinkersComponent */]
+                __WEBPACK_IMPORTED_MODULE_8__app_component__["a" /* AppComponent */],
+                __WEBPACK_IMPORTED_MODULE_9__coffee_drinker_coffee_drinker_component__["a" /* CoffeeDrinkerComponent */],
+                __WEBPACK_IMPORTED_MODULE_10__list_coffee_drinkers_list_coffee_drinkers_component__["a" /* ListCoffeeDrinkersComponent */]
             ],
             imports: [
-                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+                __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["BrowserModule"],
                 __WEBPACK_IMPORTED_MODULE_2__angular_forms__["b" /* ReactiveFormsModule */],
+                __WEBPACK_IMPORTED_MODULE_4__swimlane_ngx_datatable__["NgxDatatableModule"],
                 __WEBPACK_IMPORTED_MODULE_3__angular_common_http__["c" /* HttpClientModule */],
                 // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
                 // and returns simulated server responses.
                 // Remove it when a real server is ready to receive requests.
-                __WEBPACK_IMPORTED_MODULE_4_angular_in_memory_web_api__["a" /* HttpClientInMemoryWebApiModule */].forRoot(__WEBPACK_IMPORTED_MODULE_5__in_memory_drinker_data_service_service__["a" /* InMemoryDrinkerDataServiceService */], { dataEncapsulation: false })
+                __WEBPACK_IMPORTED_MODULE_5_angular_in_memory_web_api__["a" /* HttpClientInMemoryWebApiModule */].forRoot(__WEBPACK_IMPORTED_MODULE_6__in_memory_drinker_data_service_service__["a" /* InMemoryDrinkerDataServiceService */], { dataEncapsulation: false })
             ],
-            providers: [__WEBPACK_IMPORTED_MODULE_9__coffee_drinkers_service__["a" /* CoffeeDrinkersService */], __WEBPACK_IMPORTED_MODULE_5__in_memory_drinker_data_service_service__["a" /* InMemoryDrinkerDataServiceService */]],
-            bootstrap: [__WEBPACK_IMPORTED_MODULE_6__app_component__["a" /* AppComponent */]]
+            providers: [__WEBPACK_IMPORTED_MODULE_11__coffee_drinkers_service__["a" /* CoffeeDrinkersService */], __WEBPACK_IMPORTED_MODULE_6__in_memory_drinker_data_service_service__["a" /* InMemoryDrinkerDataServiceService */], __WEBPACK_IMPORTED_MODULE_7__intra_component_message_service_service__["a" /* IntraComponentMessageServiceService */]],
+            bootstrap: [__WEBPACK_IMPORTED_MODULE_8__app_component__["a" /* AppComponent */]]
         })
     ], AppModule);
     return AppModule;
@@ -167,6 +173,7 @@ module.exports = "<p>\n  coffee-drinker works!\n</p>\n<label>Name: <input type='
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_forms__ = __webpack_require__("../../../forms/esm5/forms.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__coffee_drinkers_service__ = __webpack_require__("../../../../../src/app/coffee-drinkers.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__intra_component_message_service_service__ = __webpack_require__("../../../../../src/app/intra-component-message-service.service.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -179,9 +186,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
+
 var CoffeeDrinkerComponent = /** @class */ (function () {
-    function CoffeeDrinkerComponent(drinkersService) {
+    function CoffeeDrinkerComponent(drinkersService, intraMessages) {
         this.drinkersService = drinkersService;
+        this.intraMessages = intraMessages;
         this.name = new __WEBPACK_IMPORTED_MODULE_1__angular_forms__["a" /* FormControl */]('');
     }
     CoffeeDrinkerComponent.prototype.ngOnInit = function () {
@@ -191,15 +200,18 @@ var CoffeeDrinkerComponent = /** @class */ (function () {
         var name = this.name.value;
         var contributions = 5.0;
         console.log("name updated to " + name);
-        this.drinkersService.addDrinker({ name: name, contributions: contributions });
+        var newDrinker = { name: name, contributions: contributions };
+        this.drinkersService.addDrinker(newDrinker);
+        this.intraMessages.sendMessage(newDrinker); // ugly!?!
     };
     CoffeeDrinkerComponent = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
             selector: 'app-coffee-drinker',
             template: __webpack_require__("../../../../../src/app/coffee-drinker/coffee-drinker.component.html"),
             styles: [__webpack_require__("../../../../../src/app/coffee-drinker/coffee-drinker.component.css")]
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2__coffee_drinkers_service__["a" /* CoffeeDrinkersService */]])
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_2__coffee_drinkers_service__["a" /* CoffeeDrinkersService */],
+            __WEBPACK_IMPORTED_MODULE_3__intra_component_message_service_service__["a" /* IntraComponentMessageServiceService */]])
     ], CoffeeDrinkerComponent);
     return CoffeeDrinkerComponent;
 }());
@@ -238,10 +250,12 @@ var CoffeeDrinkersService = /** @class */ (function () {
         return this.http.get(this.drinkersUrl);
     };
     CoffeeDrinkersService.prototype.addDrinker = function (drinker) {
+        console.log('coffee-drinkers.service.addDrinker:');
+        console.log(JSON.stringify(drinker));
         return this.http.post(this.drinkersUrl, drinker, httpOptions);
     };
     CoffeeDrinkersService = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* Injectable */])(),
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__angular_common_http__["b" /* HttpClient */]])
     ], CoffeeDrinkersService);
     return CoffeeDrinkersService;
@@ -276,9 +290,51 @@ var InMemoryDrinkerDataServiceService = /** @class */ (function () {
         return { drinkers: drinkers };
     };
     InMemoryDrinkerDataServiceService = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* Injectable */])()
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])()
     ], InMemoryDrinkerDataServiceService);
     return InMemoryDrinkerDataServiceService;
+}());
+
+
+
+/***/ }),
+
+/***/ "../../../../../src/app/intra-component-message-service.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return IntraComponentMessageServiceService; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_rxjs_Subject__ = __webpack_require__("../../../../rxjs/_esm5/Subject.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+var IntraComponentMessageServiceService = /** @class */ (function () {
+    function IntraComponentMessageServiceService() {
+        this.subject = new __WEBPACK_IMPORTED_MODULE_1_rxjs_Subject__["a" /* Subject */]();
+    }
+    IntraComponentMessageServiceService.prototype.sendMessage = function (drinker) {
+        this.subject.next({ drinker: drinker });
+    };
+    IntraComponentMessageServiceService.prototype.clearMessage = function () {
+        this.subject.next();
+    };
+    IntraComponentMessageServiceService.prototype.getMessage = function () {
+        return this.subject.asObservable();
+    };
+    IntraComponentMessageServiceService = __decorate([
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Injectable"])(),
+        __metadata("design:paramtypes", [])
+    ], IntraComponentMessageServiceService);
+    return IntraComponentMessageServiceService;
 }());
 
 
@@ -306,7 +362,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/list-coffee-drinkers/list-coffee-drinkers.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<p>\n  list-coffee-drinkers works!\n</p>\n<ul>\n  <li *ngFor='let drinker of coffeeDrinkers'>{{drinker.name}}</li>\n</ul>"
+module.exports = "<p>\n  list-coffee-drinkers works!\n</p>\n<ul>\n  <li *ngFor='let drinker of coffeeDrinkers'>{{drinker.name}} contributed {{drinker.contributions | currency}}</li>\n</ul>\n\n<div>\n   <ngx-datatable\n       [rows]=\"coffeeDrinkers\"\n       [columns]=\"columns\">\n       <!-- Failed to get notes from \n        https://github.com/swimlane/ngx-datatable/issues/401\n        to work... \n       <ngx-datatable-column prop=\"contributions\">\n         <ng-template let-value=\"contributions\" ngx-datatable-cell-template>\n           {{ contributions | currency }}\n         </ng-template>\n       </ngx-datatable-column>\n       -->\n    </ngx-datatable>\n</div>\n\n<button (click)=\"getDrinkers()\">getDrinkers()</button>"
 
 /***/ }),
 
@@ -317,6 +373,7 @@ module.exports = "<p>\n  list-coffee-drinkers works!\n</p>\n<ul>\n  <li *ngFor='
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return ListCoffeeDrinkersComponent; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/esm5/core.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__coffee_drinkers_service__ = __webpack_require__("../../../../../src/app/coffee-drinkers.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__intra_component_message_service_service__ = __webpack_require__("../../../../../src/app/intra-component-message-service.service.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -328,25 +385,46 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 
 
+
 var ListCoffeeDrinkersComponent = /** @class */ (function () {
-    function ListCoffeeDrinkersComponent(coffeeDrinkersService) {
+    function ListCoffeeDrinkersComponent(coffeeDrinkersService, intraMessages) {
+        var _this = this;
         this.coffeeDrinkersService = coffeeDrinkersService;
+        this.intraMessages = intraMessages;
+        this.columns = [{ prop: 'name' }, { prop: 'contributions' }];
+        this.subscription = intraMessages.getMessage().subscribe(function (drinker) {
+            _this.drinker = drinker.drinker;
+            console.log(_this.drinker);
+            _this.coffeeDrinkers.push(_this.drinker);
+            // https://github.com/swimlane/ngx-datatable/issues/934
+            _this.coffeeDrinkers = _this.coffeeDrinkers.slice();
+            // Doesn't work -- Why!? this.getDrinkers(); // Since only message is for new drinker 
+        });
     }
     ListCoffeeDrinkersComponent.prototype.ngOnInit = function () {
         this.getDrinkers();
         console.log(JSON.stringify(this.coffeeDrinkers));
     };
+    ListCoffeeDrinkersComponent.prototype.ngOnDestroy = function () {
+        // unsubscribe to ensure no memory leaks
+        this.subscription.unsubscribe();
+    };
     ListCoffeeDrinkersComponent.prototype.getDrinkers = function () {
         var _this = this;
-        this.coffeeDrinkersService.getDrinkers().subscribe(function (drinkers) { return _this.coffeeDrinkers = drinkers; });
+        this.coffeeDrinkersService.getDrinkers().subscribe(function (drinkers) {
+            _this.coffeeDrinkers = drinkers;
+            console.log('list-coffee-drinkers coffeeDrinkers are:');
+            console.log(JSON.stringify(_this.coffeeDrinkers));
+        });
     };
     ListCoffeeDrinkersComponent = __decorate([
-        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
+        Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
             selector: 'app-list-coffee-drinkers',
             template: __webpack_require__("../../../../../src/app/list-coffee-drinkers/list-coffee-drinkers.component.html"),
             styles: [__webpack_require__("../../../../../src/app/list-coffee-drinkers/list-coffee-drinkers.component.css")]
         }),
-        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__coffee_drinkers_service__["a" /* CoffeeDrinkersService */]])
+        __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1__coffee_drinkers_service__["a" /* CoffeeDrinkersService */],
+            __WEBPACK_IMPORTED_MODULE_2__intra_component_message_service_service__["a" /* IntraComponentMessageServiceService */]])
     ], ListCoffeeDrinkersComponent);
     return ListCoffeeDrinkersComponent;
 }());
@@ -385,7 +463,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_7" /* enableProdMode */])();
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["enableProdMode"])();
 }
 Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */])
     .catch(function (err) { return console.log(err); });

1401bec04b175b4a15d94aa5ff6ed72ea722c117
Matt Payne <Payne@MattPayne.org>
2018-07-23 05:16:07 -0500

    Copied from http://jasonwatmore.com/post/2016/12/01/angular-2-communicating-between-components-with-observable-subject

--- src/app/intra-component-message-service.service.ts
+++ src/app/intra-component-message-service.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Subject } from 'rxjs/Subject';
 
 @Injectable()
 export class IntraComponentMessageServiceService {
-
+  private subject = new Subject<any>();
+ 
   constructor() { }
+
+  sendMessage(message: string) {
+      this.subject.next({ text: message });
+  }
+
+  clearMessage() {
+      this.subject.next();
+  }
+
+  getMessage(): Observable<any> {
+      return this.subject.asObservable();
+  }
+
 
 }
